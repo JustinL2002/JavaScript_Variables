bcd06520d4646854957d65ee06b704818c8ab614
Justin Lawson <jmlawson2002@gmail.com>
2022-10-17 14:34:32 -0400

    JavaScript variables

--- app.js
+++ app.js
@@ -4,7 +4,27 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Justin";
+let lastName = "Lawson";
+var age = 20;
 
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+
+let title = "Name of the Winf";
+let author = "Patrick Rothfuss";
+let pageCount = 772;
+let bookMark = 456;
+let hasRead = true;
 // Exercise 2
 
 // Exercise 3
